fb314422df7f51f9e513b902b62ee3a9b2d73450
Deryabin Roman <46558697+DeryabinRS@users.noreply.github.com>
2025-04-29 17:11:43 +0700

    add menu

---
+++ database/migrations/2025_04_29_075805_create_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->default(NULL);
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('menus')->cascadeOnDelete();
+            $table->string('icon', 255)->default(NULL);
+            $table->string('order')->default(0);
+            $table->string('slug', 255)->default(NULL);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
